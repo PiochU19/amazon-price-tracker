5804b366c343bef7f0265b9cfc9ff22e94dfea68
PiochU19 <792954018@wp.pl>
2021-06-02 11:58:30 +0200

    local Docker modified, Custom User Model among with Custom User Manager created
    * entrypoint.local.sh flush db during local building
    * Custom User Model with email instead of username

--- django/amazon_price_tracker/account/models.py
+++ django/amazon_price_tracker/account/models.py
@@ -1,3 +1,49 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom User Manager where user, and superuser is created
+    based on email, not username.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_employee", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    """
+    Custom User Model with
+    email instead of username
+    """
+
+    email = models.EmailField(unique=True)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- django/config/settings.py
+++ django/config/settings.py
@@ -162,3 +162,8 @@ CELERY_RESULT_BACKEND = "redis://redis:6379"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_SERIALIZER = "json"
+
+
+# Pointing to custom User Model
+
+AUTH_USER_MODEL = "account.User"
