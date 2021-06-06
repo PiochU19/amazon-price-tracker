01dca5ee0a9616b991806426e4d22d0cf94881d9
PiochU19 <792954018@wp.pl>
2021-06-07 01:09:03 +0200

    API registration validation tested

--- django/amazon_price_tracker/account/tests/test_views.py
+++ django/amazon_price_tracker/account/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.http import response
 import pytest
 from faker import Faker
 
@@ -38,3 +39,40 @@ def test_user_register_201_created_and_400_due_email(client, db):
     response = client.post(url, data)
     assert response.status_code == 400
     assert response.data["email"][0] == "user with this email already exists."
+
+
+def test_user_register_400(client, db):
+    url = reverse("account:user-list")
+    data = {
+        "email": faker.email(),
+        "first_name": faker.first_name(),
+        "last_name": faker.last_name(),
+        "password": "Givemejobplz1!",
+        "password_confirmation": "Givemejobplz123!",
+    }
+    response = client.post(url, data)
+    ## two different passwords
+    assert response.status_code == 400
+    assert response.data["password"][0] == "passwords don't match"
+
+    data["password"], data["password_confirmation"] = "badpass", "badpass"
+    response = client.post(url, data)
+    ## invalid password
+    assert response.status_code == 400
+    assert response.data["password"][0] == "invalid password"
+
+
+def test_get_user_data_403(client, db):
+    url = reverse("account:user-list")
+    response = client.get(url)
+    assert response.status_code == 403
+
+
+def test_get_user_data_200(client, user):
+    url = reverse("account:user-list")
+    client.force_login(user)
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.data["email"] == user.email
+    assert response.data["first_name"] == user.first_name
+    assert response.data["last_name"] == user.last_name
